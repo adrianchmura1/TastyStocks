162baa41c39699ed1dceea81fa4c94e1bd019ff6
Adrian Chmura <adrianchmura990@gmail.com>
2023-09-15 12:46:03 +0200

    Initial Commit

---
+++ TastyStocks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TastyStocks
+//
+//  Created by Adrian Chmura on 15/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
